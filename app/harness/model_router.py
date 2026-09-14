121270b4ec4cc1a0b6f1c744225401dc07e9e963
Antriksh Rana <antrikshrana187@gmail.com>
2026-09-14 19:17:51 +0530

    feat: add model router

---
+++ app/harness/model_router.py
@@ -0,0 +1,17 @@
+class ModelRouter:
+    def __init__(self, registry):
+        self.task_type = {
+            "reasoning" : ["gemini","gpt"],
+            "coding" : ["gpt","gemini"],
+            "data_analysis" : ["qwen","gemini"]
+        }
+        self.registry = registry
+        
+    def route(self, task_type):
+        
+        for model_name in self.task_type[task_type]:
+            model = self.registry.get(model_name)
+            if model.is_available():
+                return model
+            
+        raise RuntimeError("No model available")
